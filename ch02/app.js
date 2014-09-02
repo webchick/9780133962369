c9f2a8a06279c9faecaeddfd4f04c79e61cd3896
webchick <drupal@webchick.net>
2014-09-01 23:14:01 -0700

    Adding a simple node.js server.

---
+++ ch02/app.js
@@ -0,0 +1,6 @@
+var http = require("http");
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(3000);

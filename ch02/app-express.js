d17d37694ad2afe0f9eee8b52eb2e8e41ba57938
webchick <drupal@webchick.net>
2014-09-01 23:22:52 -0700

    Fix deprecated error.

--- ch02/app-express.js
+++ ch02/app-express.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-  response.send(200, 'Hello World');
+  response.status(200).send('Hello World');
 });
 
 app.listen(8888);
